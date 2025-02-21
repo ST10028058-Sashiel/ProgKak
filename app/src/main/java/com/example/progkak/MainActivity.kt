d918bf85e7a4b955b209c9503f76470c9f533ab7
ST10028058-Sashiel <128804235+ST10028058-Sashiel@users.noreply.github.com>
2025-02-21 09:44:25 +0200

    huihi

--- app/src/main/java/com/example/progkak/MainActivity.kt
+++ app/src/main/java/com/example/progkak/MainActivity.kt
@@ -21,32 +21,26 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
-       btnOne = findViewById(R.id.button)
+        btnOne = findViewById(R.id.button)
         ValOne = findViewById(R.id.edtTextVal1)
         ValTwo = findViewById(R.id.edtTextVal2)
         Result = findViewById(R.id.textView)
 
-//
-//       val int Num1 = ValOne
-//     val int Num2 = ValTwo
-
-
-
-    }
-
-
-    private fun add(Num1:Int ,Num2 :Int ){
-        val Result = Num1+Num2
+        btnOne.setOnClickListener {
+            val num1 = ValOne.text.toString().toDoubleOrNull() ?: 0.0
+            val num2 = ValTwo.text.toString().toDoubleOrNull() ?: 0.0
+            val sum = num1 + num2
+            Result.text = "Result: $sum"
+        }
 
 
     }
-
 }
+
+
+
+
+
+
