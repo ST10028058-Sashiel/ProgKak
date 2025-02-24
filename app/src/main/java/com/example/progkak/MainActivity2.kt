31251a49275940a78a020d32cd9b78549a83cbc2
ST10028058-Sashiel <128804235+ST10028058-Sashiel@users.noreply.github.com>
2025-02-24 09:48:20 +0200

    mmm

---
+++ app/src/main/java/com/example/progkak/MainActivity2.kt
@@ -0,0 +1,32 @@
+package com.example.progkak
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity2 : AppCompatActivity() {
+
+    private  lateinit var btnNext :Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main2)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        btnNext = findViewById(R.id.btnNext)
+
+        btnNext.setOnClickListener{
+            val i = Intent( this,MainActivity::class.java)
+            startActivity(i)
+    }
+    }
+}
